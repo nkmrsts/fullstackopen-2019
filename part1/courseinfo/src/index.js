2a1d29d641ddf8bf6efbc2d8583ccf70b5dafdb8
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2019-11-07 14:54:03 +0900

    exercises part1b courseinfo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -18,37 +18,44 @@ const Part = (props) => {
 const Content = (props) => {
   return(
   <>
-    <Part part={props.payload.part1} exercises={props.payload.exercises1} />
-    <Part part={props.payload.part2} exercises={props.payload.exercises2} />
-    <Part part={props.payload.part3} exercises={props.payload.exercises3} />
+    <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+    <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+    <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
   </>
   )
 }
 
 const Total = (props) => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
   return (
-    <p>Number of exercises {props.payload.exercises1 + props.payload.exercises2 + props.payload.exercises3}</p>
+    <p>Number of exercises {total}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content payload={
-        {part1, part2, part3, exercises1, exercises2, exercises3}
-      }/>
-      <Total payload={
-        {exercises1, exercises2, exercises3}
-        }/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }

8042cd83ebc9d93805440dd900bd8ace3742e6af
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-01-28 19:57:17 +0900

    exercise part3-c2

--- exercise/server/index.js
+++ exercise/server/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-
-const cors = require("cors");
-app.use(cors());
-
-app.use(express.static("build"));
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+const cors = require("cors");
+app.use(cors());
 
 const morgan = require("morgan");
 app.use(morgan("common"));
@@ -21,50 +20,25 @@ const requestLogger = (request, response, next) => {
 };
 app.use(requestLogger);
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  }
-];
+const Person = require("./models/person");
+
+app.use(express.static("build"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then(persons => {
+    res.json(persons.map(person => person.toJSON()));
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(person => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.id).then(person => {
+    res.json(person.toJSON());
+  });
 });
-
-const generateRandomNumber = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
@@ -75,19 +49,14 @@ app.post("/api/persons", (req, res) => {
     });
   }
 
-  if (persons.find(person => person.name === body.name)) {
-    return res.status(400).json({
-      error: "name must be unique"
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    id: generateRandomNumber(1, 100)
-  };
-  persons = persons.concat(person);
-  res.json(person);
+    number: body.number
+  });
+
+  person.save().then(savedPerson => {
+    res.json(savedPerson.toJSON());
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
@@ -98,10 +67,12 @@ app.delete("/api/persons/:id", (req, res) => {
 });
 
 app.get("/api/info", (req, res) => {
-  res.send(`
+  Person.find({}).then(persons => {
+    res.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
   `);
+  });
 });
 
 const unknownEndpoint = (request, response) => {

dacfffe938751cee0634bdd3000e7d808a090577
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-01-28 21:02:53 +0900

    exercise part3-c3

--- exercise/server/index.js
+++ exercise/server/index.js
@@ -28,23 +28,40 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/persons", (req, res) => {
+app.get("/api/persons", (request, response) => {
   Person.find({}).then(persons => {
-    res.json(persons.map(person => person.toJSON()));
+    response.json(persons.map(person => person.toJSON()));
   });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    res.json(person.toJSON());
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person.toJSON());
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+  const person = { name, number };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON());
+    })
+    .catch(error => next(error));
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
 
   if (!body.name || !body.number) {
-    return res.status(400).json({
+    return response.status(400).json({
       error: "The name or number is missing"
     });
   }
@@ -55,20 +72,21 @@ app.post("/api/persons", (req, res) => {
   });
 
   person.save().then(savedPerson => {
-    res.json(savedPerson.toJSON());
+    response.json(savedPerson.toJSON());
   });
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id);
-
-  res.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
-app.get("/api/info", (req, res) => {
+app.get("/api/info", (request, response) => {
   Person.find({}).then(persons => {
-    res.send(`
+    response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
   `);
@@ -79,7 +97,19 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+// handler of requests with result to errors
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {

9c4842e8dc7ee129699bd76e332e0306664880fa
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-01-29 12:10:10 +0900

    study part3-d1

--- study/server/index.js
+++ study/server/index.js
@@ -59,7 +59,7 @@ app.put("/api/notes/:id", (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
@@ -74,9 +74,13 @@ app.post("/api/notes", (request, response) => {
     date: new Date()
   });
 
-  note.save().then(savedNote => {
-    response.json(savedNote.toJSON());
-  });
+  note
+    .save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote);
+    })
+    .catch(error => next(error));
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -98,6 +102,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError" && error.kind === "ObjectId") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
   next(error);
 };

566c8e8d06a91a15d6bfddc5248ed5679f711bf5
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-01-29 17:03:13 +0900

    exercise eslint fix

--- exercise/server/models/person.js
+++ exercise/server/models/person.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
-mongoose.set("useFindAndModify", false);
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+mongoose.set('useFindAndModify', false)
 
-const url = process.env.MONGODB_URI;
-console.log("connecting to", url);
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
 
 mongoose
   .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
-    console.log("connected to MongoDB");
+    console.log('connected to MongoDB')
   })
   .catch(error => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -26,15 +26,15 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     required: true
   }
-});
-personSchema.plugin(uniqueValidator);
+})
+personSchema.plugin(uniqueValidator)
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   }
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)

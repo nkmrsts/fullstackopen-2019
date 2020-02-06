6ae6cc5adaecbc1cb35063131b177d268febe9b1
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-02-06 15:46:49 +0900

    study part4-b

---
+++ note-app/server/tests/note_api.test.js
@@ -0,0 +1,105 @@
+const supertest = require('supertest')
+const mongoose = require('mongoose')
+mongoose.set('useFindAndModify', false)
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Note = require('../models/note')
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+
+  const noteObjects = helper.initialNotes
+    .map(note => new Note(note))
+  const promiseArray = noteObjects.map(note => note.save())
+  await Promise.all(promiseArray)
+})
+
+test('notes are returned as json', async () => {
+  console.log('entered test')
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/notes')
+
+  expect(response.body.length).toBe(helper.initialNotes.length)
+})
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb()
+
+  const noteToView = notesAtStart[0]
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(resultNote.body).toEqual(noteToView)
+})
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb()
+  const noteToDelete = notesAtStart[0]
+
+  await api
+    .delete(`/api/notes/${noteToDelete.id}`)
+    .expect(204)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  expect(notesAtEnd.length).toBe(
+    helper.initialNotes.length - 1
+  )
+
+  const contents = notesAtEnd.map(r => r.content)
+
+  expect(contents).not.toContain(noteToDelete.content)
+})
+
+test('a valid note can be added ', async () => {
+  const newNote = {
+    content: 'async/await simplifies making async calls',
+    important: true,
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(201)
+    // .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+
+  const notesAtEnd = await helper.notesInDb()
+  expect(notesAtEnd.length).toBe(helper.initialNotes.length + 1)
+
+  const contents = notesAtEnd.map(n => n.content)
+  expect(contents).toContain(
+    'async/await simplifies making async calls'
+  )
+})
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true
+  }
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(400)
+
+  const notesAtEnd = await helper.notesInDb()
+
+  expect(notesAtEnd.length).toBe(helper.initialNotes.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})

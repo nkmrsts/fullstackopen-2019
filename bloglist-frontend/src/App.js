5240b3d3ccd6532feb9a5715a794af8a5831a5c1
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-03-03 19:46:47 +0900

    excercise 5.8：sort blogPost

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -105,6 +105,10 @@ function App() {
       })
   }
 
+  const handleSort = () => {
+    setBlogs(blogs.slice().sort((a, b) => b.likes - a.likes))
+  }
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -155,6 +159,7 @@ function App() {
           createNewBlog={createNewBlog}
         />
       </Togglable>
+      <button onClick={handleSort}>sort</button>
 
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} handleClickLike={likeBlog} />

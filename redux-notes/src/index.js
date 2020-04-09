e233fab70ce52598aa8bd4f89c1c9f397f05c1ea
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-04-09 12:24:48 +0900

    study part6-b

---
+++ redux-notes/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import App from './App'
+import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'))

68aaaab12f94c41f4b46981c64c0d45d0b3dda9d
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-04-09 17:39:45 +0900

    fix: filterReducer.js

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -11,7 +11,7 @@ export const setFilter = filter => {
 const reducer = (state = "", action) => {
   switch (action.type) {
     case "SET_FILTER":
-      return action.content;
+      return action.filter;
 
     default:
       return state;

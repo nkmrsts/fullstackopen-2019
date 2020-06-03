28eee9d697f16950656a2850b7345835576967be
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-06-03 10:50:39 +0900

    excercise 8.4

--- part8/library-backend.js/index.js
+++ part8/library-backend.js/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
     hello: String!
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author]!
   }
 `
@@ -112,7 +112,12 @@ const resolvers = {
     hello: () => { return "world" },
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+      return books.filter(book => book.author === args.author)
+    },
     allAuthors: () => authors
   },
   Author: {

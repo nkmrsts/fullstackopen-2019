c0de4ac943697cfc8c06c51be93e033b140f9c5b
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2020-06-15 11:47:23 +0900

    fix: Recommend.js

--- part8/library-frontend/src/components/Recommend.js
+++ part8/library-frontend/src/components/Recommend.js
@@ -1,29 +1,47 @@
-import React from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import React, { useState, useEffect } from 'react'
+import { useApolloClient, useQuery } from '@apollo/react-hooks'
 import { FILTER_BOOKS, ME } from '../queries'
 import BooksTable from '../components/BooksTable'
 
 const Recommend = (props) => {
+  const client = useApolloClient()
   const user = useQuery(ME)
-  const books = useQuery(FILTER_BOOKS, {
-    variables: {
-      genre: user && user.data && user.data.me.favoriteGenre ?
-        user.data.me.favoriteGenre : undefined
-    }
-  })
+  const [books, setBooks] = useState(null)
+  const [filterBooksLoading, setfilterBooksLoading] = useState(false)
 
-  if (books.loading || user.loading) {
+  useEffect(() => {
+    if(user && user.data && user.data.me.favoriteGenre) {
+      const fetchBooks = async() => {
+        const filterBooks = await client.query({
+          query: FILTER_BOOKS,
+          variables: {
+            genre: user.data.me.favoriteGenre
+          }
+        })
+        setBooks(filterBooks)
+        setfilterBooksLoading(false)
+      }
+      setfilterBooksLoading(true)
+      fetchBooks()
+    }
+  }, [client, user])
+
+  if (filterBooksLoading || user.loading) {
     return <div>loading...</div>
   }
-  if(books.error || user.error) {
-    return <div>{(books.error && books.error.message )|| (user.error && user.error.message)}</div>
+  if(user.error) {
+    return <div>{(user.error && user.error.message)}</div>
+  }
+  if(books && books.error) {
+    return <div>{(books.error && books.error.message)}</div>
   }
 
   return (
     <div>
       <h2>recommendatations</h2>
       <p>book in your favorite genre { user.data.me.favoriteGenre }</p>
-      <BooksTable books={books.data.allBooks} />
+      <BooksTable books={books && books.data && books.data.allBooks ?
+            books.data.allBooks : undefined} />
     </div>
   )
 }
